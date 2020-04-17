9dce64abb4fecb114ed9cbc7b2d5f9f880e0c330
RoberVF <robertomoralesfumero@gmail.com>
2020-04-17 11:52:52 +0100

    Instala Firefox tambien

---
+++ requirements/Install_python.bat
@@ -0,0 +1,7 @@
+@echo off
+
+pause
+START https://www.python.org/ftp/python/3.7.3/python-3.7.3.exe
+START https://www.mozilla.org/es-ES/firefox/download/thanks/
+echo "Now, Python and Firefox need to be installed in your system"
+echo "After do that, run the file 'After_install_python'"
